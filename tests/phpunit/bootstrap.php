862830ed77586326d955878f6b58367bd925c6ba
Михаил Красильников <m.krasilnikov@yandex.ru>
2013-12-25 19:07:48 +0400

    В тестах сделана автозагрузка классов.

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -2,7 +2,7 @@
 /**
  * Стартовый файл тестов
  *
- * @package Eresus
+ * @package Menus
  * @subpackage Tests
  */
 
@@ -10,6 +10,25 @@
  * Путь к папке исходные кодов
  */
 define('TESTS_SRC_DIR', realpath(__DIR__ . '/../../src'));
+
+spl_autoload_register(
+    function ($class)
+    {
+        if ('Menus' == $class)
+        {
+            require TESTS_SRC_DIR . '/menus.php';
+        }
+        elseif (substr($class, 0, 6) == 'Menus_')
+        {
+            $path = TESTS_SRC_DIR . '/menus/classes/' . str_replace('_', '/', substr($class, 6))
+                . '.php';
+            if (file_exists($path))
+            {
+                require $path;
+            }
+        }
+    }
+);
 
 require_once 'stubs.php';
 
